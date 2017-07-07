7ff9f31ffa700513a6999c0609aa508a2c272d6f
Martin <martmull@hotmail.fr>
2017-07-07 13:57:13 +0200

    commit

--- test_script/test.js
+++ test_script/test.js
@@ -9,15 +9,7 @@ connection.onopen = function (session) {
 	function marketEvent (args,kwargs) {
 		console.log(args);
 	}
-	function tickerEvent (args,kwargs) {
-		console.log(args);
-	}
-	function trollboxEvent (args,kwargs) {
-		console.log(args);
-	}
-	session.subscribe('BTC_XMR', marketEvent);
-	session.subscribe('ticker', tickerEvent);
-	session.subscribe('trollbox', trollboxEvent);
+	session.subscribe('USDT_BTC', marketEvent);
 }
 
 connection.onclose = function () {
